a55f0d8e5ff0dace5fea9ae53807d7a023d22d62
LukasBarry <Lukasbarry@yahoo.com>
2018-01-03 16:09:05 -0500

    restricts routing and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'responses#new'
-  resources :analyses
-  resources :responses
+  resources :analyses, only: [:show]
+  resources :responses, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
